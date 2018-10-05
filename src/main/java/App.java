238763df449b26b7e5e2c920a79b69c44c560a21
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-05 15:28:30 +0300

    Added keyvalue to template

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+import static spark.Spark.*;
+
+import java.util.Map;
+
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/hello.vtl" );
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/favorite_photos", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/favorite_photos.vtl" );
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
+
+}
